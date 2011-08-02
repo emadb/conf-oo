6cdf3fb0f1ca7e5f5d8a7775d7bbad6344003bcb
emadb <emanuele@codiceplastico.com>
2011-08-02 08:06:48 +0200

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConfOo::Application.config.secret_token = 'f0d7246144d65d771dc1cf7246b432b56ee51a0fb13e7c3eb3d2bd17000f227eb064907c8350a8ca0ae967714f9239ae91afc35cbdf678a6e4ccfea16d34e63e'
