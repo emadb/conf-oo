7bd549ccb4b11b2530c69348599b216880804f24
emadb <emanuele@codiceplastico.com>
2011-08-05 12:46:43 +0200

    refactoring al controller

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,61 +1,45 @@
 class ProposalsController < ApplicationController
-  before_filter :authenticate_user!, :except => [:new, :create]
+  before_filter :authenticate_user!, :except => [:new, :create, :show]
+  
   def index
     @proposals = Proposal.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @proposals }
-    end
   end
 
-  # GET /proposals/1
-  # GET /proposals/1.json
   def show
     @proposal = Proposal.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @proposal }
-    end
   end
 
-  # GET /proposals/new
-  # GET /proposals/new.json
   def new
     @proposal = Proposal.new
-
-    respond_to do |format|
-      format.html 
-      format.json { render json: @proposal }
-    end
   end
 
-  # POST /proposals
-  # POST /proposals.json
   def create
     @proposal = Proposal.new(params[:proposal])
     @proposal.submitted_on = DateTime.new
-    respond_to do |format|
-      if @proposal.save
-        format.html { redirect_to @proposal, notice: 'Proposal was successfully created.' }
-        format.json { render json: @proposal, status: :created, location: @proposal }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @proposal.errors, status: :unprocessable_entity }
-      end
+    if @proposal.save
+      redirect_to @proposal, notice: 'Proposal was successfully created.' 
+    else
+      render action: "new" 
     end
   end
+  
+  def edit
+    @proposal = Proposal.find(params[:id])
+  end
+  
+  def update
+    @proposal = Proposal.find(params[:id])
 
-  # DELETE /proposals/1
-  # DELETE /proposals/1.json
+    if @proposal.update_attributes(params[:proposal])
+      redirect_to(proposals_path, :notice => 'Salvataggio effettuato.') 
+    else
+      render :action => "edit"
+    end
+    
+  end
+
   def destroy
     @proposal = Proposal.find(params[:id])
     @proposal.destroy
-
-    respond_to do |format|
-      format.html { redirect_to proposals_url }
-      format.json { head :ok }
-    end
   end
 end
