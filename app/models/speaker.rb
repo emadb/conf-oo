94622e972c1c402928afa1407fc745c2caee87ae
emadb <emanuele@codiceplastico.com>
2011-08-05 18:11:01 +0200

    Added form to save speaker infos

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -4,5 +4,6 @@ class Speaker
   field :bio
   field :web_site
   field :twitter
-  embedded_in :speech
+  embedded_in :speech, :inverse_of => :speaker
+  accepts_nested_attributes_for :speaker 
 end
