cd36ac4c95f5b863ffdc1b642db952b8519e3ba0
emadb <emanuele@codiceplastico.com>
2011-11-11 18:40:15 +0100

    Revert "aggiunte key per twitter"
    This reverts commit 6924bcaf369a8d4ecafef55a4bed7d37ff610549.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'GEMKYg2mc8HL4YbL6dbhGQ', 'tdwfvrxHQCy7KSR1scXHvrbBavBtVeFcR2WWEXAJw'
+  provider :twitter, 'key', 'secret'
 end
