9ecc2c3bd918e9e88dc3b20563ea4fcdb79b864c
emadb <emanuele@codiceplastico.com>
2011-11-11 18:30:47 +0100

    implemented twitter auth

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,12 +1,20 @@
 class PoolsController < ApplicationController
+	before_filter :authenticate_user!, :except =>[:login]
+	#before_filter :correct_user?, :except =>[:login]
+
+	def login
+
+	end
+
 	def index
 		@proposals = Proposal.all
 	end
 
 	def save
 		proposals = params[:proposals]
-		
-		current_user_id = 1
+
+		current_user_id = current_user.uid
+
 		vote_count = Vote.count(conditions: { user_id: current_user_id })
 		if vote_count == 0
 			proposals.each do |p|
