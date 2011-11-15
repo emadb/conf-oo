b4e9d7375f3046bf4720a62910f24b8e6a443d22
emadb <emanuele@codiceplastico.com>
2011-11-15 16:11:54 +0100

    added infos on user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
 	def create
-	  auth = request.env["omniauth.auth"]
+    auth = request.env["omniauth.auth"]
 	  user = User.where(:provider => auth['provider'], 
 	                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
 	  session[:user_id] = user.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User
   field :name
   field :provider
   field :uid
-  field :email, :type => String
+  field :email
+  field :nickname
+  field :location
+  field :image
 
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_uniqueness_of :name, :case_sensitive => false
+
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :email, :provider, :uid
   attr_protected :provider, :uid, :name, :email
 
@@ -21,8 +25,15 @@ class User
 	    if auth['info']
 	    	user.name = auth['info']['name'] || ""
 	      user.email = auth['info']['email'] || ""
+        user.nickname = auth['info']['nickname'] || ""
+        user.location = auth['info']['location'] || ""
+        user.image = auth['info']['image'] || ""
 	    end
 	  end
 	end
+
+  def is_admin
+    true
+  end
 
 end
