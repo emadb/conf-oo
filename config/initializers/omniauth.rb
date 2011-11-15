90d741ba1bd75e74d7ec45e5bf0dac4523dfc72e
emadb <emanuele@codiceplastico.com>
2011-11-15 17:40:18 +0100

    tuning & refactoring

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'key', 'secret'
+  provider :twitter, 'GEMKYg2mc8HL4YbL6dbhGQ', 'tdwfvrxHQCy7KSR1scXHvrbBavBtVeFcR2WWEXAJw'
 end
