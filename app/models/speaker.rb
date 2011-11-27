5a25f1e54281c4ee7af1ad651fc8c622d17793ea
emadb <emanuele@codiceplastico.com>
2011-11-27 13:51:10 +0100

    resolved issue with twitter link

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -9,4 +9,13 @@ class Speaker
   
   embedded_in :embedded_speaker, polymorphic: true
   accepts_nested_attributes_for :speaker
+
+  def twitter_link
+  	unless twitter.nil?
+	  	if !twitter.starts_with? 'http'
+				return "http://twitter.com/#!/#{twitter}"
+			end
+		end
+		twitter
+	end
 end
