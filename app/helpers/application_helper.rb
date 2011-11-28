9039533810877315aaff4efdf4af1290a852195b
emadb <emanuele@codiceplastico.com>
2011-11-28 09:32:25 +0100

    removed empty link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
 	def image_link(link, image)
-    raw "<a href='#{link}'><img src='#{image}' /></a>"
+		if !link.empty?
+    	raw "<a href='#{link}'><img src='#{image}' /></a>"
+    end
   end
 end
