52e451a875568c299cf1381901c9fc31109b88f8
emadb <emanuele@codiceplastico.com>
2011-12-06 21:23:54 +0100

    mail template and time check for the registration page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,11 @@ class SessionsController < ApplicationController
     if current_user.is_admin?
       redirect_to admin_area_url
     else
-      redirect_to polls_url, :notice => "Signed in!"
+      if (Time.now < Time.new(2011, 12, 12, 11, 00))
+        redirect_to polls_url
+      else
+        redirect_to new_attendee_url
+      end
     end
 	end
 
