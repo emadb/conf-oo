f6a6e4fea2ee724b1ef7a297ecc180218a8606f6
emadb <emanuele@codiceplastico.com>
2011-12-06 22:05:42 +0100

    moved configuration to YML

--- config/initializers/conf_oo_config.rb
+++ config/initializers/conf_oo_config.rb
@@ -1,2 +1,2 @@
-ConfOo::Application.config.open_registration_date = Time.new(2011, 12, 12, 11, 12)
-ConfOo::Application.config.max_attendees = 150
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/conf_oo_config.yml")[Rails.env]
