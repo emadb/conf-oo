a31eb62c0e08b19038506ace67ae16318100d0ae
simonech <simone@piyosailing.com>
2011-12-08 13:54:14 +0100

    Added   config.email_sender configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ ConfOo::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.email_sender="info@ugialt.net"
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
