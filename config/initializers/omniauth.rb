abb2cff93702252938e4282649740e5dcfcbb932
Claudio Maccari <claudio@jitidea.net>
2011-12-11 22:15:06 +0100

    key secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'GEMKYg2mc8HL4YbL6dbhGQ', 'tdwfvrxHQCy7KSR1scXHvrbBavBtVeFcR2WWEXAJw'
-  provider :facebook, '172907689472610','24c455d09d59ce69afa3a82cb0a4c3b1'
+  provider :twitter, 'key', 'secret'
+  provider :facebook, 'key', 'secret'
 end
 
