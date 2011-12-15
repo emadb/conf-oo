bc9a303fc77542bd56f5a1958f75cf51dd324309
emadb <emanuele@codiceplastico.com>
2011-12-15 14:52:32 +0100

    completed attendee_controller and ENV variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'GEMKYg2mc8HL4YbL6dbhGQ', 'tdwfvrxHQCy7KSR1scXHvrbBavBtVeFcR2WWEXAJw'
-  provider :facebook, '172907689472610','24c455d09d59ce69afa3a82cb0a4c3b1'
+  provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret']
 end
 
 
